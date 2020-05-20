6ff68f391da9460bf927a6788788e48f090bd425
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-20 10:59:58 -0300

    criando model de produto

--- 03_node_js/node-api/server.js
+++ 03_node_js/node-api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o app
 const app = express();
 
 // iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi',
+  { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 
 // primeira rota
 app.get('/', (req, res) => {
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with react native',
+    url: 'http://github.com/facebook/react-native'
+  });
+
+
   res.send('Hello World!')
 });
 
