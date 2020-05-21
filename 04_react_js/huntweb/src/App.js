959e22db47f89baf3eea7933fd3175b7a5d08bce
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-21 18:43:05 -0300

    configurando navegação

--- 04_react_js/huntweb/src/App.js
+++ 04_react_js/huntweb/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
-import Header from './components/Header';
-import Main from './pages/main';
+import Routes from './routes';
 
 import './styles.css';
+
+import Header from './components/Header';
+// import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
