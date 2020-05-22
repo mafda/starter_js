a243cb65a91d171e466e206f843e17dde7819ec5
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-22 16:29:26 -0300

    buscando produtos da api

--- 05_react_native/huntmobile/src/pages/main.js
+++ 05_react_native/huntmobile/src/pages/main.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import { View, Text } from 'react-native';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: 'JSHunt'
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const responses = await api.get('/products');
+
+    const { docs } = response.data;
+
+    console.log(docs);
+
   };
 
   render() {
