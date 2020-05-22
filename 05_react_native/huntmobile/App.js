afcf8fe070653d21ca8f09d04f6a8c4858b583c4
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-22 15:49:35 -0300

    configurando navegação

--- 05_react_native/huntmobile/App.js
+++ 05_react_native/huntmobile/App.js
@@ -1,22 +1,9 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-// export default function App() {
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
-    );
-  }
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
