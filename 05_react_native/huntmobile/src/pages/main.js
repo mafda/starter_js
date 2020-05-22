a456a2bb9078ba1635dd1734e4bef81d72a7574f
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-22 18:40:41 -0300

    estilizando lista de produtos

--- 05_react_native/huntmobile/src/pages/main.js
+++ 05_react_native/huntmobile/src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Main extends Component {
   static navigationOptions = {
@@ -26,12 +26,12 @@ export default class Main extends Component {
   };
 
   renderItem = ({ item }) => (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>{item.description}</Text>
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
 
-      <TouchableOpacity onPress={() => { }}>
-        <Text>Acessar</Text>
+      <TouchableOpacity style={styles.productButton} onPress={() => { }}>
+        <Text sttyle={styles.productButtonText}>Acessar</Text>
       </TouchableOpacity>
     </View>
 
@@ -49,3 +49,53 @@ export default class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa'
+  },
+
+  list: {
+    padding: 20
+  },
+
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    margin: 10
+  },
+
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24
+  },
+
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: 'bold'
+  }
+});
